e890c12a8265a4ab668678be718e263f5cb59c69
baeksikoo <baeksikoo3622@gmail.com>
2022-01-24 01:55:01 +0900

    과제 제출

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,14 +1,74 @@
-const userid = document.querySelector(".userid")
-const username = document.querySelector(".username")
-const pw = document.querySelector(".password")
-const pw_check = document.querySelector(".password_check")
-const btn = document.querySelector(".btn")
+const form = $(".form")
+
+const userid = $(".userid")
+const userid_l = $(".userid_l")
+const username = $(".username")
+const username_l = $(".username_l")
+const pw = $(".password")
+const pw_l = $(".password_l")
+const pw_check = $(".password_check")
+const pw_check_l = $(".password_check_l")
+const btn1 = $(".btn1")
+const btn2 = $(".btn2")
 
 
 
-function register(e) {
-  event.preventDefault();
-  if (pw != pw_check) {
-    pw.classList.add('warning');
-  }
-}
+$(document).ready(function() {
+
+  //register
+
+  btn1.click(function(){
+    event.preventDefault()
+    if(userid.val() == "") {
+      userid.focus()
+      userid_l.addClass("warning")
+      setTimeout(() => {
+        userid_l.removeClass("warning")
+      }, 2000)
+    } 
+    else if (username.val() == "") {
+      username.focus()
+      username_l.addClass("warning")
+      setTimeout(() => {
+        username_l.removeClass("warning")
+      }, 2000);
+    } 
+    else if (pw.val() == "") {
+      pw.focus()
+      pw_l.addClass("warning")
+      setTimeout(() => {
+        pw_l.removeClass("warning")
+      }, 2000);
+    } 
+    else if (pw.val().length < 7) {
+      pw.focus()
+      pw_l.addClass("warning")
+      setTimeout(() => {
+        pw_l.removeClass("warning")
+      }, 2000);
+    } 
+  })
+
+  //login
+
+  btn2.click(function(){
+    event.preventDefault()
+    if(userid.val() == "") {
+      userid.focus()
+      userid_l.addClass("warning")
+      setTimeout(() => {
+        userid_l.removeClass("warning")
+      }, 2000)
+    }
+    else if (pw.val() == "") {
+      pw.focus()
+      pw_l.addClass("warning")
+      setTimeout(() => {
+        pw_l.removeClass("warning")
+      }, 2000);
+    } else {
+      form.submit();
+    }
+  })
+
+})
--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var userdata = require('../config/account.json')
 
 /* GET home page. */
 router.get('/:', function(req, res, next) {
@@ -8,12 +9,20 @@ router.get('/:', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('account/login');
 });
-router.use('/login_process', function(req, res, next) {
+router.post('/login_process', function(req, res, next) {
+  var post = req.body
+  var userid = post.userid
+  var pw = post.password
+  if (userid === userdata.userid && pw === userdata.password) {
+    
+  } else {
+    res.send(`NONONO`)
+  }
 });
 router.get('/register', function(req, res, next) {
   res.render('account/register');
 });
-router.use('/register_process', function(req, res, next) {
+router.post('/register_process', function(req, res, next) {
 });
 
 module.exports = router;
